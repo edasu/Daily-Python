3454a4710b59434ed33b6980dcad04c8fce54fad
Sueda Ciftci <44266522+edasu@users.noreply.github.com>
2022-01-21 22:31:50 +0000

    Update isLucky.py

--- isLucky/isLucky.py
+++ isLucky/isLucky.py
@@ -12,7 +12,7 @@ def solution(n):
 
 #OR
 def solution(n):
-    strn = str(n)
-    first_h = sum([int(x) for x in list(strn[:int(len(strn) / 2)])])
-    second_h = sum([int(x) for x in list(strn[int(len(strn) / 2):])])
-    return first_h == second_h
+    s = str(n)
+    pivot = len(s)//2
+    left, right = s[:pivot], s[pivot:]
+    return sum(map(int, left)) == sum(map(int, right))
